e22383499c3e888ac9c7b2e8850fd388bfb9e9fb
Rafał Olszewicz <rafaoski@gmail.com>
2017-02-05 21:41:28 +0100

    zmiana paginacji

--- comments.php
+++ comments.php
@@ -8,57 +8,49 @@
  <?php endif; ?>
 */
 
-// comments
-$limit = 3; // ILOŚĆ KOMENTARZY
-$start = ($input->pageNum - 1) * $limit;
-$comments = $page->comments->slice($start, $limit);
+          // comments
+          $limit = 5; // ILOŚĆ KOMENTARZY
+          $start = ($input->pageNum - 1) * $limit;
+          $comments = $page->comments->slice($start, $limit);
+              
+           if ($page->comments) {        
+            $bl_com = $comments->render(array(
+                'headline' => '<h3>Komentarze</h3>',
+                'commentHeader' => 'Dodał {cite} w dn. {created} {stars} {votes}',
+                'dateFormat' => 'm/d/y - H:i',
+                'encoding' => 'UTF-8',
+                'admin' => false, // shows unapproved comments if true
+                'replyLabel' => 'Odpowiedz',
+              ));
+            
+            $com_f = $page->comments->renderForm(array(
+                'headline' => '<h2>Dołącz Do Dyskusji</h2>',
+                'pendingMessage' => 'Twoj komentarz musi zostać zatwierdzony Przez Admina',
+                'successMessage' => 'Dzięki Twoj Komentarz został zapisany',
+                'errorMessage' => 'Wystąpily Bledy i komentarz nie został zatwierdzony',
+                'attrs' => array(
+                'id' => 'CommentForm',
+                'action' => './',
+                'method' => 'post',
+                'class' => 'comm-form',
+                'rows' => 5,
+                'cols' => 50,
+                ),
+                'labels' => array(
+                        'cite' => 'Imie',
+                        'email' => 'Twoj E-Mail',
+                        'text' => 'Komentarz',
+                        'submit' => 'Wyślij',
+                    ),
+                ));
+                echo $bl_com;
+                echo $com_f;
+          }
 
-
- if ($page->comments) {
-
-  $bl_com = $comments->render(array(
-      'headline' => '<h3>Komentarze</h3>',
-      'commentHeader' => 'Dodał {cite} w dn. {created} {stars} {votes}',
-      'dateFormat' => 'm/d/y - g:i',
-      'encoding' => 'UTF-8',
-      'admin' => false, // shows unapproved comments if true
-      'replyLabel' => 'Odpowiedz',
-      ));
-
-  $com_f = $page->comments->renderForm(array(
-      'headline' => '<h2>Dołącz Do Dyskusji</h2>',
-      'pendingMessage' => 'Twoj komentarz musi zostać zatwierdzony Przez Admina',
-      'successMessage' => 'Dzięki Twoj Komentarz został zapisany',
-      'errorMessage' => 'Wystąpily Bledy i komentarz nie został zatwierdzony',
-      'processInput' => true,
-      
-      'attrs' => array(
-      'id' => 'CommentForm',
-      'action' => './',
-      'method' => 'post',
-      'class' => 'comm-form',
-      'rows' => 5,
-      'cols' => 50,
-      ),
-
-      'labels' => array(
-              'cite' => 'Imie',
-              'email' => 'Twoj E-Mail',
-              'text' => 'Komentarz',
-              'submit' => 'Wyślij',
-          ),
-      ));
-
-      echo $bl_com;
-      echo $com_f;
-}
-
-
-  if($input->pageNum > 1) {
-      echo "<a href='./page" . ($input->pageNum - 1) . "'>Previous comments</a> ";
-  }
-
-  if($start + $limit < count($page->comments)) {
-      echo "<a href='./page" . ($input->pageNum + 1) . "'>Next Comments</a> ";
-  }
-?>
+            if($input->pageNum > 1) {
+                echo "<a href='./page" . ($input->pageNum - 1) . "'>&laquo; Poprzedni</a> ";
+            }
+            if($start + $limit < count($page->comments)) {
+                echo "<a href='./page" . ($input->pageNum + 1) . "'>Następny &raquo;</a> ";
+            }
+    ?>
